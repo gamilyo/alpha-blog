02bacbf232c4766b4478c326904f774ef027bc94
Gamil <gamil.41995@gmail.com>
2018-12-10 16:51:51 +0200

    user from

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
+  has_secure_password
   has_many :articles
   before_save { self.email = email.downcase}
   VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 validates :email , presence: true , uniqueness: { case_sensitive: false}, length: {maximum: 105}, format: {with: VALID_EMAIL_REGEX}
 validates :username , presence: true ,uniqueness: { case_sensitive: false}, length: {minimum:3 , maximum:25}
-has_secure_password
+
 end
