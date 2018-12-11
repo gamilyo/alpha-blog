4dc480a760de0b2cfe5d6cf83107355fe5966321
Gamil <gamil.41995@gmail.com>
2018-12-11 14:10:41 +0200

    ss

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+
+def index
+ @users = User.all
+
+
+end
+
   def new
    @user = User.new
   end
@@ -29,8 +36,14 @@ class UsersController < ApplicationController
      render 'edit'
 
     end
+  end
 
-   end
+def show
+  @user = User.find(params[:id])
+  
+end
+
+
 
   private
    def user_params
